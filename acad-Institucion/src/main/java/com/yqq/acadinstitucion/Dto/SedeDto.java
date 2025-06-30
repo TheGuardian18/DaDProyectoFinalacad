a4f1282eab27c5d3aafb26da6007d97d5d131e33
elvisjsAtlas1 <chat02elvisjs@gmail.com>
2025-06-30 18:42:49 -0500

    Primer commit del proyecto final DaD

---
+++ acad-Institucion/src/main/java/com/yqq/acadinstitucion/Dto/SedeDto.java
@@ -0,0 +1,6 @@
+package com.yqq.acadinstitucion.Dto;
+
+public class SedeDto {
+    private Long idSede;
+    private String nombreSede;
+}
---
+++ acad-Institucion/src/main/java/com/yqq/acadinstitucion/Servicio/InstitucionServicio.java
@@ -0,0 +1,15 @@
+package com.yqq.acadinstitucion.Servicio;
+
+import com.yqq.acadinstitucion.Dto.InstitucionDto;
+import com.yqq.acadinstitucion.Entity.Institucion;
+
+import java.util.List;
+
+public interface InstitucionServicio {
+
+    InstitucionDto save(Institucion institucion);
+
+    List<InstitucionDto> getAll();
+
+    InstitucionDto getById(Integer id);
+}
